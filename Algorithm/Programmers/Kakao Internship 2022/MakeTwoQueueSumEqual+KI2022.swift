d1c4089f404bebb9e14342ecceb228d9fa335601
LeeJungHo <swift.adorable@gmail.com>
2022-09-17 17:05:23 +0900

    프로그래머스 두 큐 합 같게 만들기 Kakao Internship 2022

---
+++ Algorithm/Programmers/Kakao Internship 2022/MakeTwoQueueSumEqual+KI2022.swift
@@ -0,0 +1,115 @@
+//
+//  MakeTwoQueueSumEqual+KI2022.swift
+//  Algorithm
+//
+//  Created by Picturesque on 2022/09/17.
+//
+
+import Foundation
+
+//MARK: First - 22~24 시간 초과
+/*
+ - 원인:
+    - 배열의 크기가 커질수록 removeFirst 와 append가 시간 초과의 원인이 되는 듯하다.
+ */
+/*
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+
+ var bufQ1 = queue1
+ var bufQ2 = queue2
+
+ var sumQ1 = queue1.reduce(0, +)
+ let sumQ2 = queue2.reduce(0, +)
+
+ if (sumQ1 + sumQ2) % 2 != 0 {
+     return -1
+ }
+
+ let targetValue = (sumQ1 + sumQ2) / 2
+
+ if targetValue == sumQ1 { return 0 }
+
+ var count = 0
+
+ while (sumQ1 != targetValue) {
+
+     if count > (queue1.count*3) {
+         return -1
+     }
+
+     if sumQ1 > targetValue {
+
+         let value = bufQ1.removeFirst()
+         sumQ1 -= value
+         bufQ2.append(value)
+
+     } else {
+
+         let value = bufQ2.removeFirst()
+         sumQ1 += value
+         bufQ1.append(value)
+
+     }
+
+     count += 1
+
+ }
+
+ return count
+}
+ */
+
+//MARK: Second 통과
+/*
+ while 문이 끝나고도 목표 값과 한쪽 큐의 합이 같지 않을 경우의 조건 때문에 테스트 케이스 11, 28번 실패
+ if targetValue != sumQ1 { return -1 } 추가
+ */
+
+class MakeTwoQueueSumEqual_KI2022 {
+    func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+
+        let bufQueueAll = queue1 + queue2
+        
+        var ptLeft = 0
+        var ptRight = queue1.count
+        
+        var sumQ1 = queue1.reduce(0, +)
+        let sumQ2 = queue2.reduce(0, +)
+
+        if (sumQ1 + sumQ2) % 2 != 0 {
+            return -1
+        }
+
+        let targetValue = (sumQ1 + sumQ2) / 2
+
+        if targetValue == sumQ1 { return 0 }
+
+        var count = 0
+
+        while ptLeft <= ptRight && ptRight < bufQueueAll.count {
+
+            if sumQ1 > targetValue {
+
+                let value = bufQueueAll[ptLeft]
+                sumQ1 -= value
+                ptLeft += 1
+                
+            } else if sumQ1 < targetValue {
+
+                let value = bufQueueAll[ptRight]
+                sumQ1 += value
+                ptRight += 1
+
+            } else {
+                break
+            }
+
+            count += 1
+
+        }
+        
+        if targetValue != sumQ1 { return -1 }
+        
+        return count
+    }
+}
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -22,34 +22,10 @@ import Foundation
 //    tree.insert($0)
 //}
 
-func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var dict: [Character:Int] = ["R" : 0, "T" : 0,
-                                 "C" : 0, "F" : 0,
-                                 "J" : 0, "M" : 0,
-                                 "A" : 0, "N" : 0]
-    
-    for (survey, choice) in zip(survey, choices) {
-        let alphabet = survey.map{ $0 }
-        switch choice {
-        case 1...3:
-            let score = choice == 1 ? 3 : choice == 3 ? 1 : choice
-            dict[alphabet[0]]! += score
-        case 5...7:
-            let score = choice == 5 ? 1 : choice == 7 ? 3 : 2
-            dict[alphabet[1]]! += score
-        default:
-            continue
-        }
-    }
-    
-    var result = ""
-    result += dict["R"]! >= dict["T"]! ? "R" : "T"
-    result += dict["C"]! >= dict["F"]! ? "C" : "F"
-    result += dict["J"]! >= dict["M"]! ? "J" : "M"
-    result += dict["A"]! >= dict["N"]! ? "A" : "N"
-    return result
-}
+//let queue1 = [1, 2, 1, 2]
+//let queue2 = [1, 10, 1, 2]
 
-let value = solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
-print(value)
+let queue1 = [4, 6, 5, 1]
+let queue2 = [3, 2, 7, 2]
 
+print(solution(queue1, queue2))
